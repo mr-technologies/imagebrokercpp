23239cd5ad4331f93435c9ac83273ea4d6ed6e01
parafin <parafin@paraf.in>
2025-04-11 15:46:34 +0200

    [*] adapt to SDK changes

--- imagebrokercpp.cpp
+++ imagebrokercpp.cpp
@@ -99,7 +99,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    iff::initialize_engine(it_iff->dump());
+    iff::initialize(it_iff->dump());
 
     std::vector<std::shared_ptr<iff::chain>> chains;
     for(const auto& chain_config : *it_chains)
@@ -364,7 +364,7 @@ int main()
 
     chains.clear();
 
-    iff::finalize_engine();
+    iff::finalize();
 
     return EXIT_SUCCESS;
 }
